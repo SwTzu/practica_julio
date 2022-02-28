f3a1f104a42948bb36aeafe3382a37c0e27d3d49
ricardodg10 <ricardo.diazgo@alumnos.uv.cl>
2022-02-28 20:58:34 -0300

    LosLibertadores listo

--- Clases.py
+++ Clases.py
@@ -352,7 +352,35 @@ class Vehiculo:
         self.__infracciones_ruta_maipo = infraccionesRutaMaipo
 
     def getInfraccionesLosLibertadores(self):
-        return self.__infracciones_los_libertadores
+        infracciones_los_libertadores = self.__infracciones_los_libertadores
+
+        if(infracciones_los_libertadores==[]):
+            print('El vehiculo de patente ', self.__patente, 'no registra infracciones en Los Libertadores')
+
+        else:
+            print('\nInfracciones Los Libertadores:\n')
+
+            indice = 0
+            for i in infracciones_los_libertadores[0]:
+                print('\nInfraccion ', indice+1, ':')
+                print('Transito sin Tag Ruta: ', i[0])
+                print('Patente: ', i[1])
+                print('Vence: ', i[2])
+                print('Total: ', i[3], '\n')
+                
+                print('\nDetalle: ')
+                for k in infracciones_los_libertadores[1][indice]:
+
+                    
+                    print('\nPatente: ', k[0])
+                    print('Nombre Concesionaria: ', k[1])
+                    print('Hora de tránsito: ', k[2])
+                    print('Fecha de tránsito: ', k[3])
+                    print('Glosa Portico: ', k[4])
+                    print('Fecha de Vencimiento: ', k[5])
+                    print('Deuda: ', k[6],'\n')
+
+                indice+=1
 
     def setInfraccionesLosLibertadores(self, infraccionesLosLibertadores):
         self.__infracciones_los_libertadores = infraccionesLosLibertadores
@@ -651,6 +679,10 @@ class EncargoRobo:
                     c.append(z.text)
             #driver.close()
         return a,b,c
+
+#Falta Vehiculos Rematados
+
+#Falta Multas no Pagadas
 
 class VespucioNorte:
     def __init__(self, patenteVehiculo):
@@ -1335,7 +1367,6 @@ class RutaMaipo:
 
         return [facturados_final,noFacturados_final]
         
-#separar datos
 class LosLibertadores:
     def __init__(self, patenteVehiculo):
         self.__patenteVehiculo = patenteVehiculo
@@ -1360,12 +1391,112 @@ class LosLibertadores:
         driver.find_element_by_xpath('/html/body/app-root/app-inicia-flujo-on-line/div[1]/div[4]/div[1]/div[2]/label/span').click()
         cajaDeuda = driver.find_elements_by_xpath(
             '/html/body/app-root/app-inicia-flujo-on-line/div[1]/div[4]/div[2]/div')
-        lista=[]
+        info=''
         for i in cajaDeuda:
-            lista.append(i.text)
-        return lista
+            info = info+i.text
 
-#separar datos
+        driver.close()
+
+        if('No existen' in info):
+            return []
+
+        else:
+
+            string1_new = info.split('\n')
+            array = []
+            aux_array = []
+            for i in string1_new:
+                if('Transito sin Tag' in i):
+                    array.append(aux_array)
+                    aux_array = []
+                    aux_array.append(i)
+                else:
+                    aux_array.append(i)
+
+            array.append(aux_array)
+            array.remove(array[0])
+            
+            indice = 0
+            array_datos = []
+            aux_array_datos = []
+            array_detalles = []
+            aux_array_detalles = []
+            aux_array_detalles_2 = []
+
+            for i in array:
+                while(indice<5):
+                    aux_array_datos.append(i[indice])
+                    indice+=1
+
+                array_datos.append(aux_array_datos)
+                aux_array_datos = []
+
+                while(True):
+                    try:
+                        aux_array_detalles.append(i[indice])
+                        indice+=1
+                    except:
+                        indice=0
+                        array_detalles.append(aux_array_detalles)
+                        aux_array_detalles = []
+                        break
+
+            for i in array_datos:
+                len_array_datos = len(array_datos)
+
+                for j in range(0, len_array_datos):
+                    if('Transito' in i[j]):
+                        i[j] = (i[j].split('Ruta')[1]).strip()
+
+                    if('Vence' in i[j]):
+                        i.pop(j)
+                        len_array_datos-=1
+
+            for i in array_detalles:
+                len_array_i = len(i)
+
+                for j in range(0, len_array_i):
+                    try:
+                        k = i[j].split(':', 1)[1]
+                    except:
+                        k = i[j]
+
+                    if(':' in k):
+                        k = k.split(' ')
+
+                    if(len(k)==2 and not('-' in k)):
+                        aux_array_detalles.append(k[1])
+                        aux_array_detalles.append(k[0])
+                    else:
+                        aux_array_detalles.append(k)
+
+                aux_array_detalles_2.append(aux_array_detalles)
+                aux_array_detalles = []
+
+            array_detalles_final = []
+
+            for i in aux_array_detalles_2:
+                array_i = []
+                aux_array_i = []
+
+                for j in i:
+                    if(j==self.__patenteVehiculo):
+                        array_i.append(aux_array_i)
+                        aux_array_i = []
+                        aux_array_i.append(j) 
+                    
+                    else:
+                        aux_array_i.append(j)
+
+                array_i.append(aux_array_i)
+                array_i.remove(array_i[0])
+                array_detalles_final.append(array_i)
+
+            print(array_datos)
+            print(array_detalles_final)
+
+            return [array_datos, array_detalles_final]
+
 class ElPacifico:
     def __init__(self, patenteVehiculo):
         self.__patenteVehiculo = patenteVehiculo
@@ -1498,7 +1629,6 @@ class ElPacifico:
 
 for i in lista_patentes:
     print('\nPatente: ',i,'\n')
-    #auto = Vehiculo('JPKS44')
     auto = Vehiculo(i)
 
 # Para obtener Registro de Transporte Publico
@@ -1514,9 +1644,6 @@ for i in lista_patentes:
 
 #auto.setInfraccionesLampaSantiago(LampaSantiago(auto.getPatente()).getInfracciones())
 #auto.getInfraccionesLampaSantiago()
-
-    auto.setInfraccionesElPacifico(ElPacifico(auto.getPatente()).getInfracciones())
-    auto.getInfraccionesElPacifico()
 
 # Encargo_Robo1 = EncargoRobo(auto.getPatente(), "2a2b5480b431e8976a70ebbf3d38f550",'https://www.autoseguro.gob.cl')
 # a,b,c = Encargo_Robo1.resultado("2a2b5480b431e8976a70ebbf3d38f550",'https://www.autoseguro.gob.cl')
