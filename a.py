7e2d6dcbf0d8044ea5270736c22c2da8baedaadf
Benjamin Morales <morbemon2012@gmail.com>
2022-03-01 12:52:42 -0300

    generador de patentes

--- a.py
+++ a.py
@@ -1,28 +1,34 @@
-from selenium import webdriver
-import os
-def get_driver(name_path: str = 'chromedriver.exe', options: None = None, verbose: int = 0, debug: bool = False):
-    try:
-        chrome_path = os.path.dirname(__file__) + f'./{name_path}'
-        if verbose == 0:
-            driver = webdriver.Chrome(executable_path=chrome_path)
-        elif verbose == 1:
-            driver = webdriver.Chrome(
-                executable_path=chrome_path, options=options)
-    except:
-        chrome_path = os.path.abspath('./f{name_path}')
-        if verbose == 0:
-            driver = webdriver.Chrome(executable_path=chrome_path)
-        elif verbose == 1:
-            driver = webdriver.Chrome(
-                executable_path=chrome_path, options=options)
-    return driver
-options = webdriver.ChromeOptions()
 
-# options.add_argument('--proxy-server=%s' % PROXY[0])
-options.add_argument('--ignore-certificate-errors')
-options.add_argument("--lang=es")
-driver = get_driver(name_path='chromedriver.exe',
-                    options=options, verbose=1, debug=True)
+import requests
+from bs4 import BeautifulSoup
+headers = {
+    'authority': 'www.volanteomaleta.com',
+    'cache-control': 'max-age=0',
+    'sec-ch-ua': '"Opera GX";v="83", "Chromium";v="97", ";Not A Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'upgrade-insecure-requests': '1',
+    'origin': 'https://www.volanteomaleta.com',
+    'content-type': 'application/x-www-form-urlencoded',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36 OPR/83.0.4254.70',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-user': '?1',
+    'sec-fetch-dest': 'document',
+    'referer': 'https://www.volanteomaleta.com/',
+    'accept-language': 'en-US,en;q=0.9,es-CL;q=0.8,es;q=0.7',
+    'cookie': '_ga=GA1.2.125922202.1646149308; _gid=GA1.2.656936339.1646149308; _gat_gtag_UA_158657120_1=1',
+}
+with open("gen-ruts-1646148869994.txt",'r') as f:
+    ruts = f.readlines()
+    for i in ruts:
+        i=(i.strip('\n').replace(',','.'))
+        data = {
+        'term': i
+        }
 
-link1 = 'https://www.registrocivil.cl/OficinaInternet/web/carro.srcei'
-driver.get(link1)
+        response = requests.post('https://www.volanteomaleta.com/rut', headers=headers, data=data)
+        soup=BeautifulSoup(response.text, 'html.parser')
+        for x in soup.find('table',class_='table table-hover').find('tbody').find_all('tr'):
+            print(x.find_all('td')[0].text,file=open('patentes.txt','a'))
